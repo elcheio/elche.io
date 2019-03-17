66dd8b82c760e511613dc909d0a58888725b7120
SImon Koch <agorilla@me.com>
2019-03-17 22:00:10 +0100

    Added video atom

---
+++ src/ui/atoms/video/index.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import Video from './video.vue';
+
+Vue.component('a-video', Video);
---
+++ src/ui/atoms/video/video.vue
@@ -0,0 +1,36 @@
+<template functional>
+    <video
+        class="a-video"
+        :class="{ 'a-video--full-screen': props.fullScreen }"
+        :autoplay="props.autoplay"
+        :loop="props.loop"
+    >
+        <source :src="`/content/videos/${props.src}`" type="video/mp4">
+    </video>
+</template>
+
+<script>
+    export default {
+        props: {
+            src: {
+                type: String,
+                required: true,
+            },
+            autoplay: {
+                type: Boolean,
+                required: false,
+                default: false,
+            },
+            loop: {
+                type: Boolean,
+                required: false,
+                default: false,
+            },
+            fullScreen: {
+                type: Boolean,
+                required: false,
+                default: false,
+            },
+        }
+    };
+</script>
