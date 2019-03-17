fc36ce1b4cea119e30ab34d0176d12b9915aff5f
Simon Koch <simon.koch@cybercon.de>
2019-03-17 22:00:32 +0100

    Added navigation molecule

---
+++ src/ui/molecules/navigation/index.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import Navigation from './navigation.vue';
+
+Vue.component('m-navigation', Navigation);
---
+++ src/ui/molecules/navigation/navigation.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="m-navigation">
+        <ul class="m-navigation__inner">
+            <li class="m-navigation__item">
+                <a-link-box href="/work/" :hover="true">Work</a-link-box>
+            </li>
+            <li class="m-navigation__item">
+                <a-link-box href="/blog/" :hover="true">Blog</a-link-box>
+            </li>
+            <li class="m-navigation__item">
+                <a-link-box href="/about/" :hover="true">About</a-link-box>
+            </li>
+        </ul>
+    </nav>
+</template>
