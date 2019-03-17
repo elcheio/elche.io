1ec0dd3f58d10f7cb994ed8c9f0442aeebf6d657
SImon Koch <agorilla@me.com>
2019-03-17 22:01:57 +0100

    Added header organism

---
+++ src/ui/organisms/header/header.vue
@@ -0,0 +1,12 @@
+<template>
+    <header class="o-header t-inner">
+        <a-link-box
+            size="l"
+            color="secondary"
+        >
+            ELX.dev
+        </a-link-box>
+
+        <m-navigation class="o-header__navigation"></m-navigation>
+    </header>
+</template>
